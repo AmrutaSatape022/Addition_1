784deab2039f06c73e42ebbe902af23099043996
AmrutaSatape022 <167702535+AmrutaSatape022@users.noreply.github.com>
2024-08-20 20:44:40 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+public class Example 
+{
+    public static void main(String [] args)
+    {
+        int x=10;
+        int y=20;
+        x=x+y;
+        System.out.println("Value of x="+x);
+    }
+}
